f04771e18f447ab6b41b6d5e8687b1f9985217af
brinkqiang <brink.qiang@gmail.com>
2022-04-20 20:12:53 +0800

    add

--- examples/websocket_server_test.cpp
+++ examples/websocket_server_test.cpp
@@ -13,18 +13,18 @@
 #include "htime.h"
 #include "hssl.h"
 
-/*
- * #define TEST_WSS 1
- *
- * @build   ./configure --with-openssl && make clean && make
- *
- * @server  bin/websocket_server_test 9999
- *
- * @client  bin/websocket_client_test ws://127.0.0.1:9999/
- *          bin/websocket_client_test wss://127.0.0.1:10000/
- *
- */
-#define TEST_WSS 0
+ /*
+  * #define TEST_WSS 1
+  *
+  * @build   ./configure --with-openssl && make clean && make
+  *
+  * @server  bin/websocket_server_test 9999
+  *
+  * @client  bin/websocket_client_test ws://127.0.0.1:9999/
+  *          bin/websocket_client_test wss://127.0.0.1:10000/
+  *
+  */
+#define TEST_WSS 1
 
 using namespace hv;
 
@@ -57,7 +57,7 @@ int main(int argc, char** argv) {
         MyContext* ctx = channel->newContext<MyContext>();
         // send(time) every 1s
         ctx->timerID = setInterval(1000, [channel](TimerID id) {
-            char str[DATETIME_FMT_BUFLEN] = {0};
+            char str[DATETIME_FMT_BUFLEN] = { 0 };
             datetime_t dt = datetime_now();
             datetime_fmt(&dt, str);
             channel->send(str);
@@ -82,8 +82,8 @@ int main(int argc, char** argv) {
     server.https_port = port + 1;
     hssl_ctx_init_param_t param;
     memset(&param, 0, sizeof(param));
-    param.crt_file = "cert/server.crt";
-    param.key_file = "cert/server.key";
+    param.crt_file = "../../cert/server.crt";
+    param.key_file = "../../cert/server.key";
     param.endpoint = HSSL_SERVER;
     if (hssl_ctx_init(&param) == NULL) {
         fprintf(stderr, "hssl_ctx_init failed!\n");
